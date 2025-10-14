b27701b7c95364c29b168a605d69a1c4ba2f86a3
Lucas23s <lucaseduardog48@gmail.com>
2025-10-14 20:32:50 -0300

    Criação das ViewModels e Injeção de Dados no HomeController

---
+++ GCook/ViewModels/LoginVM.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GCook.ViewModels;
+
+public class LoginVM
+{
+    [Display(Name = "Email ou Nome de Usuário", Prompt = "Informe seu Email ou Nome de Usuário")]
+    [Required(ErrorMessage = "O Email ou Nome de Usuário é obrigatório")]
+    public string Email { get; set;}
+    
+    [DataType(DataType.Password)]
+    [Display(Name = "Senha de Acesso", Prompt = "********")]
+    [Required(ErrorMessage = "A Senha é obrigatória")]
+    public string Senha { get; set;}
+    
+    [Display(Name = "Manter Conectado?")]
+    public bool Lembrar { get; set;} = false;
+    public string UrlRetorno { get; set;}
+}
